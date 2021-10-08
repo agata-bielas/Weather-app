b652f8cac56c2651f0e8f2ea26529d38cc6cf87b
agata-bielas <bielas.agata@gmail.com>
2021-10-08 22:10:29 +0200

    Add functions with Connect database and create table in file db.py

---
+++ db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+DATABASE_NAME = "weather.sqlite3"
+
+
+def get_db():
+    conn = sqlite3.connect(DATABASE_NAME)
+    return conn
+
+
+def create_table():
+    db = get_db()
+    cursor = db.cursor()
+    statement_sql = 'CREATE TABLE IF NOT EXISTS weather_conditions(' \
+                    'id INTEGER PRIMARY KEY AUTOINCREMENT, ' \
+                    'temp REAL, ' \
+                    'temp_feels_like REAL, ' \
+                    'wind_speed REAL, ' \
+                    'data TEXT)'
+
+    cursor.execute(statement_sql)
